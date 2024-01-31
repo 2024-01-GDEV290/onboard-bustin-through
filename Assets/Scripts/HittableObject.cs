38d30a46ca059bb614ed77a2052c4b6425dfa1ea
Eric Brown <90017825+ericbrown8787@users.noreply.github.com>
2024-01-30 20:39:26 -0500

    Orient decal front towards side of glass pane that player is on
    Particle effects(when added) will reliably face player. Also flipped decal on one side so it looks properly mirrored.

--- Assets/Scripts/HittableObject.cs
+++ Assets/Scripts/HittableObject.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class HittableObject : MonoBehaviour
@@ -18,18 +19,23 @@ public class HittableObject : MonoBehaviour
         currentHealth = maxHealth;
     }
 
-    public void ShowDamage(Vector3 pos) {
-        GameObject GO = Instantiate(hitDecal, pos, Quaternion.identity);
-        GO.transform.SetParent(transform);
-        GO.transform.localRotation = Quaternion.identity; // Resetting the local rotation of the decal object so it appears on the surface of the glass regardless of angle. 
-        //Destroy(GO, 5);
+    public void ShowDamage(Vector3 pos, Vector3 hitOrigin) 
+    {
+        GameObject decal = Instantiate(hitDecal, pos, Quaternion.identity);
+        decal.transform.SetParent(transform);
+        decal.transform.localRotation = Quaternion.identity; // Resetting the local rotation of the decal object so it appears on the surface of the glass regardless of angle.
+        Vector3 diff = hitOrigin - pos;
+        if (Vector3.Angle(diff, pos) > 90){
+            // Orienting the decal to the correct direction so the particle system is facing towards the side the player is on. 
+            decal.transform.localEulerAngles = new Vector3(180, 0, 0);
+        }        
     }
 
     public void TakeDamage(int damage, Vector3 hitPos, Vector3 hitOrigin)
     {
         parent.PlayHitSound();
         currentHealth -= damage;
-        ShowDamage(hitPos);
+        ShowDamage(hitPos,hitOrigin);
         Debug.Log($"{this.name} has taken damage!");
         if (currentHealth <= 0)
         {
